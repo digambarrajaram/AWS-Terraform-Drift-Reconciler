f230859ecdc85a558ff5298186ec732943571345
Digambar Rajaram <digambarrajaram02@gmail.com>
2026-07-16 19:44:56 +0530

    [scope-a] Drift fix: aws_vpc_security_group_ingress_rule.ssh_ingress [HIGH]

--- terraform_code/ec2_terraform_account_a/main.tf
+++ terraform_code/ec2_terraform_account_a/main.tf
@@ -89,9 +89,9 @@ resource "aws_vpc_security_group_ingress_rule" "https_ingress" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_ingress" {
   security_group_id = aws_security_group.drift_web_ssh_sg.id
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
+  from_port         = "-1"
+  to_port           = "-1"
+  ip_protocol       = "icmpv6"
   cidr_ipv4         = "192.0.2.0/24"
   description       = "SSH access from trusted corporate network"
 }

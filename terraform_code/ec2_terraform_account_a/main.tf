b94d65e03a8d3796b13daf67e0394eb8aad962ce
Digambar Rajaram <digambarrajaram02@gmail.com>
2026-08-12 20:50:56 +0530

    [scope-a] Security fix: main.tf (2 issues)

--- terraform_code/ec2_terraform_account_a/main.tf
+++ terraform_code/ec2_terraform_account_a/main.tf
@@ -23,6 +23,8 @@ resource "aws_vpc" "drift_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+  enable_classiclink   = false
+  enable_flow_log      = true
 
   tags = {
     Name = "drift-vpc"
